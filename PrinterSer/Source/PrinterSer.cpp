563e6dab9d3eb71fd603f1cd730d5c417af0692b
pikwiatk <piotr.1.kwiatkowski@nokia.com>
2020-04-09 10:16:22 +0200

    add simple handler to printer service

--- PrinterSer/Source/PrinterSer.cpp
+++ PrinterSer/Source/PrinterSer.cpp
@@ -10,9 +10,22 @@ namespace PrinterSerMsg
 {
 ID getID(const std::string& p_msg)
 {
-	return ID::Stop;
+	if(p_msg == "Stop")
+		return ID::Stop;
+	return ID::Print;
 }
 }
+
+class PrintHandler : public msg::Handler
+{
+public:
+	std::string handle(const std::string& p_msg)
+	{
+		std::cout << "  PRINT" << std::endl;
+		std::cout << p_msg << std::endl;
+		return "";
+	}
+};
 
 Printer::Printer()
 	: BaseService(std::bind(&Printer::createServer, this), &PrinterSerMsg::getID)
@@ -22,6 +35,9 @@ Printer::Printer()
 	host = addr.host;
 	port = addr.port;
 	std::cout << "PrinterService registered at [" << host << ":" << port << "]";
+
+	addHandler(PrinterSerMsg::ID::Stop, std::make_unique<BaseService::NativeStopHandler>(*this));
+	addHandler(PrinterSerMsg::ID::Print, std::make_unique<PrintHandler>());
 }
 
 std::unique_ptr<msg::Server> Printer::createServer()
