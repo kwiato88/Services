3159ff979546aa2972bd680a0edacac4e09998dd
pikwiatk <piotr.1.kwiatkowski@nokia.com>
2024-12-23 14:18:02 +0100

    Chatter: client; refactor main

--- Chatter/Client/Source/ChatterClientMain.cpp
+++ Chatter/Client/Source/ChatterClientMain.cpp
@@ -18,6 +18,60 @@ struct SockEnv
     }
 };
 
+class App
+{
+public:
+    ~App()
+    {
+        logOut();
+    }
+    void registerUser()
+    {
+        //TODO: query for user name and password
+        std::string password;
+        std::cout << "Chatter: register " << user << std::endl;
+        if(!Chatter::ClientApp::registerAtServer(user, password))
+        {
+            WinApi::MessageDialog{WinApi::Handle(0)}.withTitle("Register")
+                .withContent("Failed to register " + user).show();
+            throw std::runtime_error("Failed to register " + user);
+        }
+        WinApi::MessageDialog{WinApi::Handle(0)}.withTitle("Register")
+            .withContent("Registered " + user).show();
+    }
+    void runClient()
+    {
+        logIn();
+        std::cout << "Chatter: start client " << user << std::endl;
+        WinApi::ChatterClientDialog dlg(hModule, WinApi::Handle(0), user, cookie);
+        dlg.show();
+        std::cout << "Chatter: client " << user << " finished" << std::endl;
+    }
+    void logIn()
+    {
+        //TODO: query for user name and password
+        std::string password;
+        std::cout << "Chatter: login " << user << std::endl;
+        cookie = Chatter::ClientApp::logIn(user, password);
+        if(cookie.empty())
+        {
+            WinApi::MessageDialog{WinApi::Handle(0)}.withTitle("Login")
+                .withContent("Failed to login " + user).show();
+            throw std::runtime_error("Failed to login " + user);
+        }
+    }
+    void logOut()
+    {
+        if(!cookie.empty())
+        {
+            Chatter::ClientApp::logOut(cookie);
+        }
+    }
+private:
+    std::string user;
+    std::string cookie;
+};
+
 int main(int argc, char* argv[])
 {
     if (argc > 2)
@@ -26,38 +80,16 @@ int main(int argc, char* argv[])
         return 1;
     }
     SockEnv sockEnv;
-    if (argc == 2 && argv[1] == "register")
-    {
-        std::cout << "Chatter: register user" << std::endl;
-        //TODO: query for user name and password
-        std::string user, password;
-        if(Chatter::ClientApp::registerAtServer(user, password))
-        {
-            WinApi::MessageDialog{WinApi::Handle(0)}.withTitle("Register")
-                .withContent("Registered " + user).show();
-            return 0;
-        }
-        WinApi::MessageDialog{WinApi::Handle(0)}.withTitle("Register")
-            .withContent("Failed to register " + user).show();
-        return 1;
-    }
     try
     {
-        //TODO: query for user name and password
-        std::string user, password;
-        auto cookie = Chatter::ClientApp::logIn(user, password);
-        if(cookie.empty())
+        if (argc == 2 && argv[1] == "register")
         {
-            WinApi::MessageDialog{WinApi::Handle(0)}.withTitle("Login")
-                .withContent("Failed to login " + user).show();
-            return 1;
+            App client;
+            client.registerUser();
+            return 0;
         }
-        std::cout << "Chatter: logged " << user << std::endl;
-        std::cout << "Chatter: start client " << user << std::endl;
-        WinApi::ChatterClientDialog dlg(hModule, WinApi::Handle(0), user, cookie);
-        dlg.show();
-        std::cout << "Chatter: client " << user << " finished" << std::endl;
-        Chatter::ClientApp::logOut(cookie);
+        App client;
+        client.runClient();
         return 0;
     }
     catch (std::exception& e)
